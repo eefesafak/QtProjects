73b5ad0f8386e544a34d15e8377f93c855f608e7
eefesafak <79419141+eefesafak@users.noreply.github.com>
2022-05-13 16:31:09 +0300

    Add files via upload

--- TextFinder/main.cpp
+++ TextFinder/main.cpp
@@ -1,1 +1,11 @@
-dsasd
+#include "textfinder.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    TextFinder w;
+    w.show();
+    return a.exec();
+}
