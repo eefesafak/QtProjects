e367a0ee0be22a49f34d7d006be025a9291afa48
eefesafak <79419141+eefesafak@users.noreply.github.com>
2022-05-20 14:42:16 +0300

    Update mainwindow.h

--- CountLine/mainwindow.h
+++ CountLine/mainwindow.h
@@ -6,6 +6,9 @@
 #include <QPushButton>
 #include <QFileInfoList>
 #include <QFileDialog>
+#include <QtConcurrentRun>
+#include <QMutex>
+
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -19,15 +22,27 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
-    void addItemToList(const QString& file, const int& fileSize, const int& count);
     QFileInfoList getFileListFromDir(const QString& directory);
+    void process(const QString& path);
+    bool getReturnState();
+    void setReturnState(bool state);
+
+signals:
+    void processFinished(int sum);
+    void addItemToList(const QString& file, const int& fileSize, const int& count);
+
+public slots:
+    void slotProcessFinished(int sum);
+    void slotAddItemToList(const QString& file, const int& fileSize, const int& count);
+private:
+    bool m_return;
+    QMutex m_mutex;
+    Ui::MainWindow *ui;
+    countLine* m_ig;
 
 private slots:
     void on_Browse_clicked();
     void on_Ok_clicked();
-
-private:
-    Ui::MainWindow *ui;
-    countLine* m_ig;
+    void on_cancel_clicked();
 };
 #endif // MAINWINDOW_H
